31482ef03d12a452a4fda855ad7fb6681aece084
mkhuselityhobeka@gmail.com <mkhuselityhobeka@gmail.com>
2021-12-11 18:34:30 +0200

    intial commit

---
+++ src/main/java/co/za/myprofileservice/MyProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package co.za.myprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/myprofileservice/MyProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.myprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
